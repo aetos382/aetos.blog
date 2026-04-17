022e85c271f37369091b6dc4224bf7fb00db10d5
aetos <aetos382@users.noreply.github.com>
2026-04-17 20:40:08 +0900

    test

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,9 @@
-bundle config set --global bin ./bin
+#!/usr/bin/env bash
+set -euo pipefail
+. /etc/profile.d/ruby-gems.sh
 
-(cd src && bundle install && bundle binstubs asciidoctor)
+bundle config set --global path.system true
+
+(cd src && bundle install)
 
 curl -fsSL https://claude.ai/install.sh | bash

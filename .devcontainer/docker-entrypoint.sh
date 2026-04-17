ad6141753b6ace18c749c2dd101df6853081bcc5
aetos <aetos382@users.noreply.github.com>
2026-04-17 20:40:08 +0900

    test

--- .devcontainer/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-export GEM_HOME="$HOME/.gem"
-export GEM_PATH="$GEM_HOME:$(ruby -r rubygems -e 'puts Gem.default_path.join(":")')"
-export PATH="$GEM_HOME/bin:$PATH"
-
-exec "$@"

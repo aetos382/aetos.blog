c7cf36a35f6063806b9d91e277d7f60d22a2a5cf
aetos <aetos382@users.noreply.github.com>
2026-04-17 21:04:29 +0900

    wip

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -7,3 +7,9 @@ bundle config set --global path.system true
 (cd src && bundle install)
 
 curl -fsSL https://claude.ai/install.sh | bash
+
+hugo version | echo "hugo: $(cat -)"
+sass --version | echo "dart-sass: $(cat -)"
+asciidoctor --version | echo "asciidoctor: $(cat -)"
+go version | echo "go: $(cat -)"
+claude --version | echo "claude code: $(cat -)"
